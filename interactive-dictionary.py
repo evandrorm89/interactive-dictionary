82f8f8d0eb8a487f91b25493ec1ce0875dc39454
evandrorm89 <evandrorm89@gmail.com>
2018-06-25 20:25:44 -0300

    Add files via upload

---
+++ interactive-dictionary.py
@@ -0,0 +1,38 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+	
+	if w.lower() in data:
+		return data[w.lower()]
+
+	elif w.capitalize() in data:
+		return data[w.capitalize()]
+
+	elif w.upper() in data:
+		return data[w.upper()]
+		
+	elif len (get_close_matches(w, data.keys())) > 0:
+		x = input("Did you mean %s instead? (y/n)" % get_close_matches(w, data.keys())[0])
+		x = x.lower()
+		if x == 'y':
+			return data[get_close_matches(w, data.keys())[0]]
+		elif x == 'n':
+			w = input ("What did you mean then? ")
+			return translate (w)
+		else:
+			return "please, enter y or n"
+	else:
+		return "The word doesn't exist"
+
+word = input ("Type a word: ")
+
+output = translate(word)
+
+if type(output) == list:
+	for i in output:
+		print (i)
+else:
+	print (output)
